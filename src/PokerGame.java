dcc7cd372771b522564673fcd40a31bc6d93b3a3
exclusive1214 <32433533+exclusive1214@users.noreply.github.com>
2018-12-29 14:58:38 +0800

    Update PokerGame.java
    blackJack

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -3,7 +3,31 @@ public class PokerGame {
 
 }
 
-class BlackJack extends PokerGame{
-  int a;
+class BlackJack extends Game{
+
+  @Override
+  void initialize() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  void play() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  boolean endGame() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  void printResult() {
+    // TODO Auto-generated method stub
+    
+  }
+ 
   
 }
