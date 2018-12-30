706a36b82f4e435bb1d32210dbcf9407672356a5
D0543027 <a0935999309@gmail.com>
2018-12-30 17:05:10 +0800

    player-com

--- src/TCGGame.java
+++ src/TCGGame.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.Scanner;
 
 public class TCGGame {
 
@@ -13,27 +14,29 @@ public class TCGGame {
 }
 
 class TCG extends Game {
-  private TCGPlayer p1;
-  private TCGPlayer p2;
+  private HumanPlayer p1;
+  private comPlayer p2;
   private int round;
 
-  @Override
   void initialize() {
     // TODO Auto-generated method stub
-    p1 = new TCGPlayer();
-    p2 = new TCGPlayer();
+    p1 = new HumanPlayer();
+    p2 = new comPlayer();
     round = 1;
   }
 
-  @Override
   void play() {
     // TODO Auto-generated method stub
     TCGCard card1;
     TCGCard card2;
     System.out.println("Round " + Integer.toString(round) + " :");
-    System.out.print("p1`s card : ");
+    p1.drawCard();
+    p2.drawCard();
+    
+    p1.ShowHandCard();
+
     card1 = p1.playCard();
-    System.out.print("p2`s card : ");
+
     card2 = p2.playCard();
 
     attack(card1, card2);
@@ -45,7 +48,6 @@ class TCG extends Game {
     round = round + 1;
   }
 
-  @Override
   boolean endGame() {
     // TODO Auto-generated method stub
     if (p1.hp <= 0 || p2.hp <= 0)
@@ -55,19 +57,29 @@ class TCG extends Game {
     return false;
   }
 
-  @Override
   void printResult() {
     // TODO Auto-generated method stub
-    if (p1.hp < 0 && p2.hp < 0)
+    if (isDraw(p1,p2))
       System.out.println("Draw!!!");
     else if (p1.hp > p2.hp)
       System.out.println("p1 win!!!");
     else
       System.out.println("p2 win!!!");
-
+  }
+  
+  private boolean isDraw(TCGPlayer p1, TCGPlayer p2) {
+    if(p1.hp < 0 && p2.hp < 0)
+      return true;
+    else if(p1.hp == p2.hp)
+      return true;
+    return false;
   }
 
   void attack(TCGCard c1, TCGCard c2) {
+    System.out.print("p1`s card : ");
+    c1.CardInfo();
+    System.out.print("p2`s card : ");
+    c2.CardInfo();
     int dmg1 = c1.hp - c2.att;
     int dmg2 = c2.hp - c1.att;
     if (dmg1 < 0)
@@ -78,11 +90,56 @@ class TCG extends Game {
 
 }
 
-class TCGPlayer {
+class HumanPlayer extends TCGPlayer {
+  Scanner sc = new Scanner(System.in);
+  
+  
+  public HumanPlayer() {
+    super();
+  }
+  
+  public void ShowHandCard() {
+    System.out.print("HandCard : ");
+    Iterator hiter = HandCard.iterator();
+    while(hiter.hasNext()) {
+      System.out.print(hiter.next());
+    }
+    System.out.println("");
+  }
+ 
+
+  public TCGCard playCard() {
+    System.out.print("choose the card you want play: ");
+    int cardSeq = sc.nextInt();
+    TCGCard c = HandCard.get(cardSeq - 1);
+    c.CardInfo();
+    HandCard.remove(cardSeq - 1);
+    return c;
+  }
+  
+
+}
+
+class comPlayer extends TCGPlayer{
+
+  public comPlayer() {
+    super();
+  }
+
+  public TCGCard playCard() {
+    TCGCard c = HandCard.get(0);
+    HandCard.remove(0);
+    return c;
+    
+  }
+
+}
+
+abstract class TCGPlayer {
 
   int hp;
   ArrayList<TCGCard> deck = new ArrayList<TCGCard>();
-
+  ArrayList<TCGCard> HandCard = new ArrayList<TCGCard>();
   Iterator<TCGCard> diter;
 
   public TCGPlayer() {
@@ -94,17 +151,20 @@ class TCGPlayer {
 
     diter = deck.iterator(); // using iterator
 
-  }
-
-  public TCGCard playCard() {
-    TCGCard c = null;
-    if (diter.hasNext()) { // Iterator
-      c = diter.next(); // play the top card from deck
-      c.CardInfo();
+    for (int i = 0; i < 5; i++) { // each player has five cards initially
+      if (diter.hasNext())
+        HandCard.add(diter.next());
     }
-    return c;
+
   }
 
+  public void drawCard() {
+    if (diter.hasNext()) {
+      HandCard.add(diter.next());
+    }
+  }
+
+  abstract TCGCard playCard();
 }
 
 class TCGCard {
@@ -120,4 +180,12 @@ class TCGCard {
   public void CardInfo() {
     System.out.println("hp :" + Integer.toString(hp) + " / att : " + Integer.toString(att));
   }
+
+  @Override
+  public String toString() {
+    // TODO Auto-generated method stub
+    return "(" + Integer.toString(hp) + "," + Integer.toString(att) + ") ";
+  }
+  
+  
 }
