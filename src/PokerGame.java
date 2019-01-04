c244ce2a8f62d21a50c7211459a22d0df7157e2e
exclusive1214 <32433533+exclusive1214@users.noreply.github.com>
2019-01-05 00:34:08 +0800

    PokerGame V.3
    若最後的分數相同 ， 則分數相同玩家各抽一張比大小 分數大的獲勝 ， 直到分出結果。程式碼註解已加上。

--- src/PokerGame.java
+++ src/PokerGame.java
@@ -11,8 +11,8 @@ public class PokerGame {
     PokerPlayer p2 = new PokerPlayer();
     PokerPlayer p3 = new PokerPlayer();
     BlackJack bjGame = new BlackJack();
-    PokerPlayer[] players = {p1,p2,p3};
-    bjGame.addPlayers(players);
+    PokerPlayer[] players = { p1, p2, p3 };
+    bjGame.addPlayers(players); // add players to the blackJack game
     bjGame.playGame();
 
   }
@@ -24,18 +24,18 @@ class BlackJack extends Game {
   int people = 0;
   private Scanner sc;
   boolean end = false;
-  
-  Deck deck = Deck.getInstance();  //Singleton
-  
+
+  Deck deck = Deck.getInstance(); // Singleton
+
   void addPlayers(PokerPlayer[] p) {
     this.players = p;
   }
-  
+
   @Override
   void initialize() {
     // TODO Auto-generated method stub
     deck.cutcards();
-    for (PokerPlayer p : players ) {
+    for (PokerPlayer p : players) { // all players draw one cards
       p.addCard(deck.deal());
       people++;
     }
@@ -47,52 +47,87 @@ class BlackJack extends Game {
     // TODO Auto-generated method stub
     sc = new Scanner(System.in);
     System.out.println("Player " + Integer.toString(turn) + "'s Trun");
-    showHandCards(players[turn-1]);
-    askCard(players[turn-1]);
-    
+    showHandCards(players[turn - 1]); 
+    askCard(players[turn - 1]); 
+
     System.out.println("------------------------------------");
   }
 
   @Override
   boolean endGame() {
     // TODO Auto-generated method stub
-    if (turn > people)
-      return true;
-    if (end == true)
+    if (turn > people || end == true) // all players finished their turn
       return true;
     return false;
   }
 
   @Override
-  void printResult() {
+  void printResult() { // print the result
     // TODO Auto-generated method stub
     System.out.println("------------------------------------");
     System.out.println("The Game is over !");
-    
+
     int temp = 1;
-    int winner = 0;
+    int[] winner = new int[10];
     int point = 0;
-    for (PokerPlayer p : players ) {
+    boolean equal = false;
+    int cont = 0;
+    for (PokerPlayer p : players) { // find the winner
       System.out.print("Player " + Integer.toString(temp) + " 'cards : ");
       p.showCards();
       System.out.println("Player " + Integer.toString(temp) + " 'points : " + Integer.toString(p.countPoint()));
-      if(p.countPoint() <= 21 && p.countPoint() > point) {
-        winner = temp;
+      if (p.countPoint() <= 21 && p.countPoint() > point) {
+        equal = false;
+        cont = 0;
+        winner[cont++] = temp;
         point = p.countPoint();
+      } else if (p.countPoint() <= 21 && p.countPoint() == point) {
+        equal = true;
+        winner[cont] = temp;
       }
       temp++;
     }
-    System.out.println("The winner is Player " + Integer.toString(winner) + " ! ");
-      
+    if (equal == true) { // if two or more players have the same points then they need to draw another to
+                         // battle, the points bigger one is the winner.
+      int[] points = new int[10];  //record points the player got in this battle
+      int[] wins = new int[10]; //record the player's number
+      int cnt = 0;   //count
+      System.out.println("------------------------------------");
+      System.out.println("The points are the same , let's battle !");
+      System.out.println("Please draw card!");
+      for (int i = 0; i <= cont; i++) {
+        PokerCard p2 = deck.deal();
+        System.out.println("Player " + winner[i] + " draw : " + p2);
+        points[cnt] = p2.getPoint();
+        wins[cnt] = winner[i];
+        cnt++;
+      }
+      int bigger = 0;
+      for (int i = 0; i < points.length; i++) {
+        int battle = points[i];
+        if (battle > bigger) {
+          bigger = battle;
+          equal = false;
+          cont = 0;
+          winner[cont++] = wins[i];
+        } else if (battle == bigger) {
+          equal = true;
+          winner[cont] = wins[i];
+        }
+      }
+
+    }
+    System.out.println("The winner is Player " + Integer.toString(winner[0]) + " ! ");
+
   }
 
-  void showHandCards(PokerPlayer p) {
+  void showHandCards(PokerPlayer p) {   // show player's hand card and show points player got
     System.out.printf("Your cards are : ");
     p.showCards();
     System.out.println("The points you got : " + p.countPoint());
   }
 
-  void askCard(PokerPlayer p) {
+  void askCard(PokerPlayer p) {   // ask player if he want to draw another card
     System.out.println("Do you want to draw another card ? [Y/N] ");
     String Sc = sc.next();
     if (Sc.equals("Y")) {
@@ -100,11 +135,13 @@ class BlackJack extends Game {
       p.addCard(c);
       System.out.println("The card you drawed was : " + c);
     } else if (Sc.equals("N")) {
-      if(turn >= people) end = true;
-      else turn++;
+      if (turn >= people)
+        end = true;
+      else
+        turn++;
     }
     System.out.println("The points you got : " + p.countPoint());
-    if (p.countPoint() > 21) {
+    if (p.countPoint() > 21) {   //if the points is more then 21, the player lose the game
       System.out.println("Your points are bigger than 21 !!!!!");
       turn++;
     }
@@ -115,41 +152,39 @@ class BlackJack extends Game {
 // -------------------------------
 class PokerPlayer {
   ArrayList<PokerCard> handCard = new ArrayList<PokerCard>();
-  public PokerPlayer() {
 
-  }
-  
   public void showCards() {
-    for(int i = 0; i < handCard.size(); i++) {
+    for (int i = 0; i < handCard.size(); i++) {
       System.out.print(handCard.get(i) + " ");
     }
     System.out.println("");
   }
-  
+
   public int countPoint() {
     int sum = 0;
-    for(int i = 0; i < handCard.size(); i++) {
+    for (int i = 0; i < handCard.size(); i++) {
       sum = sum + handCard.get(i).getPoint();
     }
     return sum;
   }
-  
-  public void addCard(PokerCard pCard) {
+
+  public void addCard(PokerCard pCard) {  //add the card the player drew in his hand cards
     handCard.add(pCard);
   }
 }
 
-class Deck{
+class Deck {
   private static Deck uniqueInstance;
   private PokerCard[] pCard = new PokerCard[52];
   private int index;
-  
-  public static Deck getInstance() {
-    if(uniqueInstance == null) {
+
+  public static Deck getInstance() {  //use Singleton to make sure only one deck create
+    if (uniqueInstance == null) {
       uniqueInstance = new Deck();
     }
     return uniqueInstance;
   }
+
   private Deck() {
     index = 0;
     String[] type = { "Spades", "Clubs", "Diamonds", "Hearts" };
@@ -158,14 +193,14 @@ class Deck{
       for (int n = 2; n <= 10; n++) {
         pCard[count++] = new PokerCard(s, Integer.toString(n));
       }
-      pCard[count++] = new PokerCard(s,"A");
-      pCard[count++] = new PokerCard(s,"J");
-      pCard[count++] = new PokerCard(s,"Q");
-      pCard[count++] = new PokerCard(s,"K");
+      pCard[count++] = new PokerCard(s, "A");
+      pCard[count++] = new PokerCard(s, "J");
+      pCard[count++] = new PokerCard(s, "Q");
+      pCard[count++] = new PokerCard(s, "K");
     }
   }
-  
-  public void cutcards() {
+
+  public void cutcards() {  //shuffle the cards
     Random rand = new Random();
     for (int i = 0; i < 52; i++) {
       int n = rand.nextInt(52);
@@ -176,12 +211,12 @@ class Deck{
     }
   }
 
-  public PokerCard deal() {
+  public PokerCard deal() {  //deal one card
     PokerCard c = pCard[index];
     index++;
     return c;
   }
-  
+
 }
 
 // -------------------------------
@@ -193,16 +228,15 @@ class PokerCard {
     this.type = type;
     this.number = number;
   }
-  
+
   public String toString() {
-    return type+number;
+    return type + number;
   }
-  
+
   public int getPoint() {
     if (number.equals("A")) {
       return 1;
-    } else if (number.equals("J") || number.equals("Q") || number.equals("K")
-        || number.equals("10")) {
+    } else if (number.equals("J") || number.equals("Q") || number.equals("K") || number.equals("10")) {
       return 10;
     } else {
       return Integer.parseInt(number);
